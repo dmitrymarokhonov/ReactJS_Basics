e608c458d7e19292656ffc561f8cf32b02760223
Dmitry <--global>
2018-03-28 22:20:48 +0300

    commuinication between parent and child components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class Home extends React.Component {
 
@@ -6,7 +7,9 @@ export class Home extends React.Component {
         super();
         this.state = {
             age: props.initialAge,
-            status: 0
+            status: 0,
+            homeLink: "Changed Link"
+
         };
         setTimeout(() => {
             this.setState({
@@ -19,8 +22,12 @@ export class Home extends React.Component {
         this.setState({
             age: this.state.age + 3
         });
-
     }
+
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
+    }
+
     render() {
         return (
             <div>
@@ -29,13 +36,18 @@ export class Home extends React.Component {
                 <p>Status:  {this.state.status}</p>
                 <hr />
                 <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older</button>
-                {/* <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button> */}
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
 }
 
-// Home.propTypes = {
-//     name: PropTypes.string,
-//     age: PropTypes.number
-// };
+Home.PropTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    greet: PropTypes.func
+
+};
