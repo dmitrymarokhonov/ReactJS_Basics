b872f3247c27c5fb3aa82f29107bdc744a8da35c
Dmitry <--global>
2018-04-06 14:31:30 +0300

    15_React_Router-Router_setup

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,12 +3,14 @@ import React from "react";
 export const Header = (props) => {
     return (
         <nav className="navbar navbar-default">
-            <div className="container">
+            <div className="container-fluid">
                 <div className="navbar-header">
-                    <ul className="nav navbar-nav">
-                        <li><a href="#">{props.homeLink}</a></li>
-                    </ul>
+                    <a className="navbar-brand" href="#">15_React_Router-Router_setup</a>
                 </div>
+                <ul className="nav navbar-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">User</a></li>
+                </ul>
             </div>
         </nav>
     );
